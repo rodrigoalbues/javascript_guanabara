d961c808fbcc5b640d2d785d55b61fcd3db68b54
rodrigoalbues <95535393+rodrigoalbues@users.noreply.github.com>
2022-04-08 19:47:37 -0400

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+var d = 1
+while (c <= 6) {
+  console.log(`Passo ${c} para while`)
+  c++
+}
+
+do {
+  console.log(`Passo ${d} para do`)
+  d++
+} while (d <= 3)
---
+++ aula14/ambiente.js
@@ -0,0 +1,10 @@
+var c = 1
+
+while (c <= 5) {
+  console.log(`c = ${c}`)
+  c++
+}
+
+for (var d = 1; d <= 5; d++) {
+  console.log(`d = ${d}`)
+}
